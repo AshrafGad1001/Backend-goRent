5c10c6bf90dd74ef07bc2f0802d18827f65a0998
Haleem <188713125+undefinedHaleem@users.noreply.github.com>
2026-06-16 03:33:58 +0300

    Update user.route.js

--- src/modules/User/user.route.js
+++ src/modules/User/user.route.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { verifyRole } from "../../Middleware/Auth.Middleware.js";
-import { banUser, unbanUser } from "./user.controller.js";
+import { verifyRole, verifyAuth } from "../../Middleware/Auth.Middleware.js";
+import { validateCreateUser, validateUpdateUser } from "./user.validation.js";
+import { upload } from "../../utils/cloudinary.js";
+import { getAllUsers, getUserById, createUser, updateUser, deleteUser, banUser, unbanUser, } from "./user.controller.js";
 
 const router = express.Router();
-
+router.get("/", verifyRole(["admin"]), getAllUsers);
+router.get("/:id", verifyAuth, getUserById);
+router.post("/", verifyRole(["admin"]), upload.any(), validateCreateUser, createUser);
+router.put("/:id", verifyAuth, upload.any(), validateUpdateUser, updateUser);
+router.delete("/:id", verifyRole(["admin"]), deleteUser);
 router.patch("/:id/ban", verifyRole(["admin"]), banUser);
 router.patch("/:id/unban", verifyRole(["admin"]), unbanUser);
 
