997ad2af708f65d3dbed12430181e06e635af7d5
undefinedHaleem <abdelhalem.radwan010@gmail.com>
2026-06-25 11:20:46 +0300

    added ownerId field to dispute model

--- src/DB/Models/dispute.model.js
+++ src/DB/Models/dispute.model.js
@@ -12,6 +12,11 @@ const disputeSchema = new mongoose.Schema(
             ref: "User",
             required: true,
         },
+        ownerId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
         subject: {
             type: String,
             required: true,
